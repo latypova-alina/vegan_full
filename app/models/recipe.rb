99e2ade2627faef2d8e1278995733518b07c6e17
alinkin9696 <alinkin9696@mail.ru>
2016-12-03 21:47:19 +0300

    add recipe model and recipe_categories

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,8 @@
 class Recipe < ActiveRecord::Base
+  mount_uploader :image, RecipeImageUploader
+
   has_many :food_recipes
   has_many :foods, through: :food_recipes
+  belongs_to :recipe_category
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   resources :foods
   resources :categories
+  resources :recipes
+  resources :recipe_categories
   root to: "foods#index"
   get '/delete_food/:id' => 'foods#destroy', as: 'delete_food'
-
+  get '/delete_recipe/:id' => 'recipes#destroy', as: 'delete_recipe'
+  get '/delete_category/:id' => 'categories#destroy', as: 'delete_category'
+  get '/delete_recipe_category/:id' => 'recipe_categories#destroy', as: 'delete_recipe_category'
 
 end
