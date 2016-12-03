a08fc7d938a87383e809faae7884da5690f4412d
alinkin9696 <alinkin9696@mail.ru>
2016-12-03 21:48:22 +0300

    add policies

---
+++ app/policies/recipe_policy.rb
@@ -0,0 +1,17 @@
+class RecipePolicy < ApplicationPolicy
+  def create?
+    @user.administrator?
+  end
+
+  def update?
+    @user.administrator?
+  end
+
+  def edit?
+    @user.administrator?
+  end
+
+  def destroy?
+    @user.administrator?
+  end
+end
