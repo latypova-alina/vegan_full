4338a29cb775f263c7099820133d1d7b84ba164f
alinkin9696 <alinkin9696@mail.ru>
2016-12-05 18:21:16 +0300

    add name to devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
+  before_action :configure_devise_permitted_parameters, if: :devise_controller?
+
+  def configure_devise_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :password_confirmation])
+  end
 end
