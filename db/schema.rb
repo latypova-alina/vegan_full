641a80621ced44616aa58b376260c003553b2c9b
alinkin9696 <alinkin9696@mail.ru>
2017-01-21 02:47:28 +0300

    add changes to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161220171047) do
+ActiveRecord::Schema.define(version: 20170119210416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,16 @@ ActiveRecord::Schema.define(version: 20161220171047) do
   end
 
   add_index "foods", ["category_id"], name: "index_foods_on_category_id", using: :btree
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "recipe_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "likes", ["recipe_id"], name: "index_likes_on_recipe_id", using: :btree
+  add_index "likes", ["user_id"], name: "index_likes_on_user_id", using: :btree
 
   create_table "recipe_categories", force: :cascade do |t|
     t.string   "recipe_category_name"
