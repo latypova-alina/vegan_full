f2f9a4cd150279c376293f0b2aff1736dda60b48
alinkin9696 <alinkin9696@mail.ru>
2017-01-26 05:09:23 +0300

    add dislike method

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,15 @@
 class LikesController < ApplicationController
   expose :recipe
+  expose :recipes, -> {current_user.liked_recipes}
 
   def like_recipe
     current_user.liked_recipes << recipe
     redirect_to :back
   end
+
+  def dislike_recipe
+    current_user.liked_recipes.delete(recipe)
+    redirect_to :back
+  end
+
 end
