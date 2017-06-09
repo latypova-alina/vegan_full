49b70a1bf93ca70514c685b1a3417a148c791816
alinkin9696 <alinkin9696@mail.ru>
2017-06-09 18:34:56 +0300

    add factory user

---
+++ spec/factories/users.rb
@@ -0,0 +1,18 @@
+FactoryGirl.define do
+  sequence :email do |n|
+    "person#{n}@example.com"
+  end
+
+  factory :user do
+    name { Faker::Name.name }
+    email { generate(:email) }
+    password '12345678'
+    password_confirmation '12345678'
+
+    trait :admin do
+      role 'administrator'
+    end
+
+    factory :admin, traits: %i(admin)
+  end
+end
