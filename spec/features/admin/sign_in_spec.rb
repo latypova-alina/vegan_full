cbc2b3deb3245ab16e06b82865b261d7c995ab7a
alinkin9696 <alinkin9696@mail.ru>
2017-06-09 18:35:58 +0300

    add sign_in feature

---
+++ spec/features/admin/sign_in_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+feature "Sign in" do
+  let!(:admin) { create :admin }
+  let(:password) { admin.password }
+
+  before(:each) do
+    visit new_user_session_path
+    fill_in 'Email', with: admin.email
+    fill_in 'Password', with: password
+    click_button 'Sign in'
+  end
+
+  admin_links = ["Добавить товар", "Добавить рецепт", "Категории продуктов", "Категории рецептов"]
+
+  scenario "Admin signs in successfully" do
+    admin_links.each do |link|
+      expect(page).to have_content(link)
+    end
+  end
+
+end
