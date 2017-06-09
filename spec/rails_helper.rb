81085c67d0f1b901a2f04d17d36c1063c174b684
alinkin9696 <alinkin9696@mail.ru>
2017-06-09 18:37:14 +0300

    add rails helpers

---
+++ spec/rails_helper.rb
@@ -0,0 +1,32 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+require 'shoulda/matchers'
+require 'spec_helper'
+require 'rspec/rails'
+require 'spec_helper'
+require 'sunspot/rails/spec_helper'
+
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+# Checks for pending migration and applies them before tests are run.
+# If you are not using ActiveRecord, you can remove this line.
+ActiveRecord::Migration.maintain_test_schema!
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = false
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+  config.before(:each) do
+    ::Sunspot.session = ::Sunspot::Rails::StubSessionProxy.new(::Sunspot.session)
+  end
+
+  config.after(:each) do
+    ::Sunspot.session = ::Sunspot.session.original_session
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+require 'shoulda-matchers'
+require 'sunspot/rails/spec_helper'
+
+
+require "bundler/setup"
+::Bundler.require(:default, :test)
+
+::Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :active_record
+    with.library :active_model
+
+  end
+end
